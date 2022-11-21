ed4d91c9bd8e416444f0c3eb70b67b61af648a0e
AmitRazi <115696533+AmitRazi@users.noreply.github.com>
2022-11-21 12:23:25 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,19 @@
-#include <stdio.h>
-#include "my_mat.h"
-#define SIZE 100
-
 int main() {
     int matrix[SIZE][SIZE];
-    int temp = 1;
+    int temp = 0;
     int flag = 0;
     int from =0,to=0;
     char ch = ' ';
     while(!flag){
+
         scanf("%c", &ch);
         switch(ch){
             case 'A':
-                for(int i = 0 ; i < 3 ; i++){
-                    for(int j = 0 ; j < 3 ; j++){
-                        scanf("%d",&temp);
-                        matrix[i][j]=temp;
-                    }
-                }
+                fill_matrix(matrix);
                 break;
             case 'B':
-                scanf("%d",&from);
-                scanf("%d",&to);
+                scanf("%d", &from);
+                scanf("%d", &to);
                 temp = floyd_warshall(matrix,from,to);
                 if(temp!=-1)
                     printf("True\n");
@@ -30,10 +22,22 @@ int main() {
                 break;
             case 'C':
                 scanf("%d", &from);
-                scanf("%d",&to);
+                scanf("%d", &to);
                 printf("%d\n",floyd_warshall(matrix,from,to));
                 break;
             case 'D':
+                flag = 1;
+                break;
+            default:
+                break;
+        }
+    }
+    printf("\n");
+
+
+
+}
+
                 flag = 1;
                 break;
             default:
